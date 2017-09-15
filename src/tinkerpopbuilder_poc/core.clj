1c88bb0043665aab5d920114a2f522314f6b5f12
Michael McDermott <mcdermott.michaelj@gmail.com>
2017-09-15 10:19:05 -0500

    Fixed property names not being set correctly.

--- src/tinkerpopbuilder_poc/core.clj
+++ src/tinkerpopbuilder_poc/core.clj
@@ -7,8 +7,8 @@
   "Build a simple graph of The Shadow's agents and write it out to a .kryo file"
   [& args]
   (let [g (TinkerGraph/open)
-        shadow-vertex (-> g (.addVertex "The Shadow"))
-        moe-vertex (-> g (.addVertex "Moe Schrevnitz") (.addEdge "drives for" shadow-vertex (into-array [])))]
+        shadow-vertex (-> g (.addVertex (into-array ["name" "The Shadow"])))
+        moe-vertex (-> g (.addVertex (into-array ["name" "Moe Schrevnitz"])) (.addEdge "drives for" shadow-vertex (into-array [])))]
     (-> (.io g (IoCore/gryo)) (.writeGraph "the-shadow.kryo"))
   )
   )
