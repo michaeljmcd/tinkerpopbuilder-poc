ec47870302c93545f95145e97881201c280e8814
Michael McDermott <mcdermott.michaelj@gmail.com>
2017-09-15 10:44:27 -0500

    Removed extraneous operator.

--- src/tinkerpopbuilder_poc/core.clj
+++ src/tinkerpopbuilder_poc/core.clj
@@ -7,7 +7,7 @@
   "Build a simple graph of The Shadow's agents and write it out to a .kryo file"
   [& args]
   (let [g (TinkerGraph/open)
-        shadow-vertex (-> g (.addVertex (into-array ["name" "The Shadow"])))
+        shadow-vertex (.addVertex g (into-array ["name" "The Shadow"]))
         moe-vertex (-> g (.addVertex (into-array ["name" "Moe Schrevnitz"])) (.addEdge "drives for" shadow-vertex (into-array [])))]
     (-> (.io g (IoCore/gryo)) (.writeGraph "the-shadow.kryo"))
   )
