dddf2e4133cc980543d9b6e94e099ac07b96fe46
Michael McDermott <mcdermott.michaelj@gmail.com>
2017-09-15 10:11:00 -0500

    Fixed issue that caused graph always to build with sample data.

--- src/tinkerpopbuilder_poc/core.clj
+++ src/tinkerpopbuilder_poc/core.clj
@@ -1,12 +1,12 @@
 (ns tinkerpopbuilder-poc.core
-  (:import (org.apache.tinkerpop.gremlin.tinkergraph.structure TinkerFactory)
+  (:import (org.apache.tinkerpop.gremlin.tinkergraph.structure TinkerGraph)
            (org.apache.tinkerpop.gremlin.structure.io IoCore))
   (:gen-class))
 
 (defn -main
   "Build a simple graph of The Shadow's agents and write it out to a .kryo file"
   [& args]
-  (let [g (TinkerFactory/createModern)
+  (let [g (TinkerGraph/open)
         shadow-vertex (-> g (.addVertex "The Shadow"))
         moe-vertex (-> g (.addVertex "Moe Schrevnitz") (.addEdge "drives for" shadow-vertex (into-array [])))]
     (-> (.io g (IoCore/gryo)) (.writeGraph "the-shadow.kryo"))
